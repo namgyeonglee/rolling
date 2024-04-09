5eb759d3a05c986373f515f58f3cb09c52a6dac1
Erin <skaurd12323@gmail.com>
2024-04-09 13:39:15 +0900

    Feat: semi true 로 변경, 페이지 추가 생성

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { App } from "./components/App"
-import { HomePage } from "./pages/HomePage"
-import { ListPage } from "./pages/ListPage"
-import { PostPage } from "./pages/PostPage"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { App } from "./components/App";
+import { HomePage } from "./pages/HomePage";
+import { ListPage } from "./pages/ListPage";
+import { MessageCreatePage } from "./pages/MessageCreatePage";
+import { PostCreatePage } from "./pages/PostCreatePage";
+import { PostPage } from "./pages/PostPage";
 
 export function Main() {
   return (
@@ -12,10 +14,14 @@ export function Main() {
           <Route index element={<HomePage />} />
           <Route path="/list" element={<ListPage />} />
           <Route path="/post">
-            <Route index element={<PostPage />} />
+            <Route index element={<PostCreatePage />} />
+            <Route path=":postId" element={<PostPage />} />
+            <Route path=":postId">
+              <Route path="message" element={<MessageCreatePage />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
