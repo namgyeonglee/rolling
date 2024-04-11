a7d339d6d1160736f903c88977106804e77f9460
LeeSoonGu <rntnsl9280@naver.com>
2024-04-11 13:32:01 +0900

    feat: add FontStyle

---
+++ src/styles/FontStyle.js
@@ -0,0 +1,127 @@
+import { css } from "styled-components";
+
+export const Bold28 = css`
+  font-family: Pretendard;
+  font-size: 2.8rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 4.2rem
+  letter-spacing: -0.028rem;
+`;
+
+export const Bold24 = css`
+  font-family: Pretendard;
+  fonst-size: 2.4rem;
+  font-style: normal;
+  font-weight: 700;
+  font-height: 3.6rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular24 = css`
+  font-family: Pretendard;
+  fonst-size: 2.4rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 3.6rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Bold20 = css`
+  font-family: Pretendard;
+  fonst-size: 2rem;
+  font-style: normal;
+  font-weight: 700;
+  font-height: 3rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular20 = css`
+  font-family: Pretendard;
+  fonst-size: 2rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 3rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Bold18 = css`
+  font-family: Pretendard;
+  fonst-size: 1.8rem;
+  font-style: normal;
+  font-weight: 700;
+  font-height: 2.8rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular18 = css`
+  font-family: Pretendard;
+  fonst-size: 1.8rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 2.8rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Bold16 = css`
+  font-family: Pretendard;
+  fonst-size: 1.6rem;
+  font-style: normal;
+  font-weight: 700;
+  font-height: 2.6rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular16 = css`
+  font-family: Pretendard;
+  fonst-size: 1.6rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 2.6rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Bold15 = css`
+  font-family: Pretendard;
+  fonst-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  font-height: 2.2rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular15 = css`
+  font-family: Pretendard;
+  fonst-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 2.2rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Bold14 = css`
+  font-family: Pretendard;
+  fonst-size: 1.4rem;
+  font-style: normal;
+  font-weight: 700;
+  font-height: 2rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular14 = css`
+  font-family: Pretendard;
+  fonst-size: 1.4rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 2rem;
+  letter-spacing: -0.028rem;
+`;
+
+export const Regular12 = css`
+  font-family: Pretendard;
+  fonst-size: 1.2rem;
+  font-style: normal;
+  font-weight: 400;
+  font-height: 1.8rem;
+  letter-spacing: -0.014rem;
+`;
