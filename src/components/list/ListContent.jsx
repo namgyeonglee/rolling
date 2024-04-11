eadb24e7bd776bed0ec71f1af11a1f75a2ea6cb7
LeeSoonGu <rntnsl9280@naver.com>
2024-04-11 14:28:14 +0900

    Content 내용 추가

--- src/components/list/ListContent.jsx
+++ src/components/list/ListContent.jsx
@@ -1,1 +1,35 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { bold24 } from "../../styles/FontStyle";
+import RecipientCardList from "./RecipientCardList";
+
+const Section = styled.section`
+  display: column;
+  width: 100%;
+  max-width: 118rem;
+  margin: 0 auto;
+  margin-top: 5rem;
+`;
+
+const TitleContainer = styled.div``;
+const RecipientCardListContainer = styled.div``;
+
+const Title = styled.h2`
+  display: flex;
+  margin-bottom: 1.6rem;
+  ${bold24}
+`;
+
+function ListContent({ title, recipients }) {
+  return (
+    <Section>
+      <TitleContainer>
+        <Title>{title}</Title>
+      </TitleContainer>
+      <RecipientCardListContainer>
+        <RecipientCardList recipients={recipients} />
+      </RecipientCardListContainer>
+    </Section>
+  );
+}
+
+export default ListContent;
