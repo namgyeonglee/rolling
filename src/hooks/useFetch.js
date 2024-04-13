a57c45ca0d05de2597103601a83fd591ddd3eb8f
ohdong9795 <ohdong9795@naver.com>
2024-04-13 13:49:53 +0900

    feat: useApi 훅 추가

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-export const useFetch = ({ url, method, headers, body }, errorCallback) => {
+export const useFetch = ({ url, errorCallback }) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -9,21 +9,8 @@ export const useFetch = ({ url, method, headers, body }, errorCallback) => {
     (async () => {
       setLoading(true);
 
-      headers = headers
-        ? headers
-        : typeof body === "object"
-          ? { "Content-Type": "application/json" }
-          : {};
-
-      const option = {
-        method,
-        headers,
-      };
-
-      if (method === "POST") option.body = JSON.stringify(body);
-
       try {
-        const response = await fetch(url, option);
+        const response = await fetch(url, { method: 'GET' });
         const result = await response.json();
 
         setData(result);
@@ -34,7 +21,57 @@ export const useFetch = ({ url, method, headers, body }, errorCallback) => {
         setLoading(false);
       }
     })();
-  }, [url, method, body]);
+  }, [url]);
 
   return { data, loading, error };
+};
+
+export const useApi = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const sendRequest = async ({
+    url,
+    method,
+    headers,
+    body,
+    callback,
+    errorCallback,
+  }) => {
+    setLoading(true);
+    setError(null);
+
+    headers = headers
+      ? headers
+      : typeof body === 'object'
+        ? { 'Content-Type': 'application/json' }
+        : {};
+
+    const option = {
+      method,
+      headers,
+    };
+
+    if (body) option.body = JSON.stringify(body);
+
+    let response = null;
+
+    try {
+      response = await fetch(url, option);
+      if (method !== 'DELETE') {
+        const result = await response.json();
+
+        setData(result);
+      } else setData(response);
+    } catch (e) {
+      setError(e);
+      if (errorCallback) errorCallback(e);
+    } finally {
+      setLoading(false);
+      if (response?.ok && callback) callback();
+    }
+  };
+
+  return [sendRequest, data, loading, error];
 };
