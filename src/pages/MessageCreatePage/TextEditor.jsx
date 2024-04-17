40e0af8ec9cdf6dea224140a4562abce3adec94f
Erin <skaurd12323@gmail.com>
2024-04-17 14:15:52 +0900

    feat: MessageCreatePage 구현

---
+++ src/pages/MessageCreatePage/TextEditor.jsx
@@ -0,0 +1,42 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: {
+    container: [
+      [{ header: [1, 2, false] }],
+      ["bold", "underline"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+    ],
+  },
+};
+
+const formats = ["header", "bold", "underline", "list", "bullet", "indent"];
+
+export function TextEditor({ value, onChange, setIsEmpty }) {
+  const onEdit = (e) => {
+    onChange(e);
+    if (e.replace("<p><br></p>", "").length === 0) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  };
+
+  return (
+    <ReactQuill
+      style={{ height: "230px", marginBottom: "50px" }}
+      theme="snow"
+      placeholder="메세지를 남겨주세요."
+      modules={modules}
+      formats={formats}
+      value={value}
+      onChange={onEdit}
+    />
+  );
+}
