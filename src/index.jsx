3b68d11a171d1d424b8b3baec614066240f12ffe
LeeSoonGu <rntnsl9280@naver.com>
2024-04-18 22:38:47 +0900

    delete import reset.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Main } from "./Main.jsx";
 import { GlobalStyles } from "./styles/GlobalStyle.js";
-import "./styles/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
