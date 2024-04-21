e5a44343b362bb031bae11ad3c52d3c39cdec54e
LeeSoonGu <rntnsl9280@naver.com>
2024-04-22 03:29:16 +0900

    Docs: RecipientCardList structure change

--- src/pages/ListPage/ListContent.jsx
+++ src/pages/ListPage/ListContent.jsx
@@ -54,14 +54,18 @@ const Title = styled.h2`
   }
 `;
 
-export function ListContent({ title, recipients }) {
+export function ListContent({ title, recipients, setDataUrl, next }) {
   return (
     <Section>
       <TitleContainer>
         <Title>{title}</Title>
       </TitleContainer>
       <RecipientCardListContainer>
-        <RecipientCardList recipients={recipients} />
+        <RecipientCardList
+          recipients={recipients}
+          setDataUrl={setDataUrl}
+          next={next}
+        />
       </RecipientCardListContainer>
     </Section>
   );
